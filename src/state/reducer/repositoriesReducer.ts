c05da29e740ec0662c402db56440d888a914d97f
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-01 16:24:41 -0700

    Applying Action interfaces and action type enum

--- src/state/reducer/repositoriesReducer.ts
+++ src/state/reducer/repositoriesReducer.ts
@@ -4,24 +4,50 @@ interface RepositoriesState {
   data: string[];
 }
 
-interface Action {
-  type: string;
-  payload?: any;    // ? : maynot, may have, if have it, the type is any
+// interface Action {
+//   type: string;
+//   payload?: any;    // ? : maynot, may have, if have it, the type is any
+// }
+// separate each action interface
+interface SearchRepositoriesAction {
+  type: 'search_repositories';
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: 'search_repositories_success';
+  payload: string[]
+}
+
+interface SearchRepositoriesErrorAction {
+  type: 'search_repositories_error';
+  payload: string
+}
+
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
 }
 
 const reducer = (
   state: RepositoriesState,
-  action: Action)
-: RepositoriesState => {
+  action: Action
+): RepositoriesState => {
+
   // use switch statement for different cases
   switch (action.type) {
-    case 'search_repositories':
+    case ActionType.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] };
     // receive success
-    case 'search_repositories_success':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
       return { loading: false, error: null, data: action.payload };
     // if error case
-    case 'search_repositories_error':
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
       // no longer loading, error contains error message, reset empty array to data
       return { loading: false, error: action.payload, data: [] };
     default:
