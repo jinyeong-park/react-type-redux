8217ac74f956037a9e459d7a8a38dc886053a4db
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-01 17:30:13 -0700

    Adding Action creator and data request Logic

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+// async Action creator - use thunk
+export const searchRepositories = (term: string) => {
+  // call dispatch fn
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES
+    });
+
+    try {
+      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+        params: {
+          text: term
+        }
+      });
+      // look at the data shape after receiving it.
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      // dispatch actions
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names
+      })
+
+
+    } catch (err) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: err.message
+      })
+    }
+
+
+
+  }
+}
